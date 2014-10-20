b782b520fc632406a336b7fe9c2698ba6ca3ed2e
Daniel Hunt <nanokid92@gmail.com>
2014-10-20 15:45:52 -0700

    adding everything to repo

---
+++ app/src/main/java/com/example/smartadvisor/Main.java
@@ -0,0 +1,15 @@
+package com.example.smartadvisor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ *
+ */
+public class Main extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+
+    }
+}
