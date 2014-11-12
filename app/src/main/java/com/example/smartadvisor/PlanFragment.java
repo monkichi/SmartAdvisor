3827d0ee9587575d74eb10c2b0646f758e2db825
Daniel Hunt <nanokid92@gmail.com>
2014-11-11 16:21:08 -0800

    getting started on the planning algorithm

---
+++ app/src/main/java/com/example/smartadvisor/PlanFragment.java
@@ -0,0 +1,31 @@
+package com.example.smartadvisor;
+
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+
+public class PlanFragment extends Fragment {
+
+
+    public PlanFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootview = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return rootview;
+    }
+
+
+}
